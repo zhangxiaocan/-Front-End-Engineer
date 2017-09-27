9926d2f6fd93a786a3301ce78891da9a14463d68
AppleUser <appleuser@ITANY-IMAC.local>
2017-09-27 15:22:45 +0800

    第三次提交

--- src/js/2.js
+++ src/js/2.js
@@ -1,7 +1,7 @@
 function fn(){
 	alert(1111);
 };
-console.log(222);
+console.log(22gf2);
 console.log(3);
 console.log(4);
 console.log(4);
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 console.log(13242351);
 console.log(2222);
+console.log('hello world');
